66e3e7a694651fa20a1445464f25dc0473f3e614
akkushwaha7834 <akkushwaha018@gmail.com>
2023-09-13 15:37:28 +0530

    center image changed

--- index.php
+++ index.php
@@ -34,6 +34,14 @@
         <a href="">Start Broker</a>
       </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
   </div>
 </div>
 
@@ -180,7 +188,7 @@
                           </div>
                     </div>
                     <div class="col-md-12 featured-card text-center">
-                        <img src="images/block.png" alt="">
+                        <img src="images/center-image.webp" alt="" width="100%">
                     </div>
                     <div class="col-md-12">
                         <div class="card  featured-card">
