4157035004274e3b524a369cd021306bf99cdf1d
akkushwaha7834 <akkushwaha018@gmail.com>
2023-09-16 15:07:21 +0530

    Our Teams Page done

--- our-teams.php
+++ our-teams.php
@@ -19,15 +19,116 @@
 			<div class="col-md-12">
 				<h2>
                 Our Expert Crew Meet 
-           
-                <span>
                 <span>Our Leadership</span>
-                </span>
-                
                 </h2>
 
-                <p>Est sit amet facilisis magna etiam tempor orci eu lobortis.
-Eu nisl nunc mi ipsum faucibus vitae.</p>
+                <p>Est sit amet facilisis magna etiam tempor orci eu lobortis. Eu nisl nunc mi ipsum faucibus vitae.</p>
+			</div>
+		</div>
+		<div class="row team_row_two">
+			<div class="col-lg-4">
+				<div class="teams_card two">
+					<img src="images/teams-1.jpg" class="team_member_image">
+					<h3 class="teama_member_name">
+						Jacob C. Huffman
+					</h3>
+					<p class="team_member_des">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+					</p>
+					<div class="team_social">
+						<a href="#"> <i class="fa fa-facebook"></i></a>
+		            <a href="#"><i class="fa fa-twitter"></i></a>
+		            <a href="#"> <i class="fa fa-linkedin"></i></a>
+		            <a href="#"> <i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="teams_card two">
+					<img src="images/teams-1.jpg" class="team_member_image">
+					<h3 class="teama_member_name">
+						Jacob C. Huffman
+					</h3>
+					<p class="team_member_des">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+					</p>
+					<div class="team_social">
+						<a href="#"> <i class="fa fa-facebook"></i></a>
+		            <a href="#"><i class="fa fa-twitter"></i></a>
+		            <a href="#"> <i class="fa fa-linkedin"></i></a>
+		            <a href="#"> <i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="teams_card two">
+					<img src="images/teams-1.jpg" class="team_member_image">
+					<h3 class="teama_member_name">
+						Jacob C. Huffman
+					</h3>
+					<p class="team_member_des">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+					</p>
+					<div class="team_social">
+						<a href="#"> <i class="fa fa-facebook"></i></a>
+		            <a href="#"><i class="fa fa-twitter"></i></a>
+		            <a href="#"> <i class="fa fa-linkedin"></i></a>
+		            <a href="#"> <i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row team_row_two">
+			<div class="col-lg-4">
+				<div class="teams_card two">
+					<img src="images/teams-1.jpg" class="team_member_image">
+					<h3 class="teama_member_name">
+						Jacob C. Huffman
+					</h3>
+					<p class="team_member_des">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+					</p>
+					<div class="team_social">
+						<a href="#"> <i class="fa fa-facebook"></i></a>
+		            <a href="#"><i class="fa fa-twitter"></i></a>
+		            <a href="#"> <i class="fa fa-linkedin"></i></a>
+		            <a href="#"> <i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="teams_card two">
+					<img src="images/teams-1.jpg" class="team_member_image">
+					<h3 class="teama_member_name">
+						Jacob C. Huffman
+					</h3>
+					<p class="team_member_des">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+					</p>
+					<div class="team_social">
+						<a href="#"> <i class="fa fa-facebook"></i></a>
+		            <a href="#"><i class="fa fa-twitter"></i></a>
+		            <a href="#"> <i class="fa fa-linkedin"></i></a>
+		            <a href="#"> <i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="teams_card two">
+					<img src="images/teams-1.jpg" class="team_member_image">
+					<h3 class="teama_member_name">
+						Jacob C. Huffman
+					</h3>
+					<p class="team_member_des">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+					</p>
+					<div class="team_social">
+						<a href="#"> <i class="fa fa-facebook"></i></a>
+		            <a href="#"><i class="fa fa-twitter"></i></a>
+		            <a href="#"> <i class="fa fa-linkedin"></i></a>
+		            <a href="#"> <i class="fa fa-instagram"></i></a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
