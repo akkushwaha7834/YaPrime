c481090f36f035e46d4cdb420c7df1be823f772e
prashnat334434 <prashantvashisth53@gmail.com>
2023-09-16 14:37:19 +0530

    teams page left

---
+++ our-teams.php
@@ -0,0 +1,37 @@
+<?php include 'includes/header.php';?>
+<!-- teams banner -->
+<section class="teams_banner">
+<div class="container team_banner_container">
+ <div class="row">
+    <div class="col-md-12 team_banner_heading">
+        <h1 class="">Our Team</h1>
+    </div>
+ </div>
+</div>
+
+</section>
+<!-- end of teams banner -->
+
+
+<section class="teams_content">
+	<div class="container">
+		<div class="row  teams_heading">
+			<div class="col-md-12">
+				<h2>
+                Our Expert Crew Meet 
+           
+                <span>
+                <span>Our Leadership</span>
+                </span>
+                
+                </h2>
+
+                <p>Est sit amet facilisis magna etiam tempor orci eu lobortis.
+Eu nisl nunc mi ipsum faucibus vitae.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include 'includes/talk-to-us.php';?>
+<?php include 'includes/footer.php';?>
