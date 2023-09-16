654c17a8bd44050124f589de4589d26929b99965
akkushwaha7834 <akkushwaha018@gmail.com>
2023-09-16 16:44:50 +0530

    Why choose us page started

---
+++ why-choose-us.php
@@ -0,0 +1,90 @@
+<?php include 'includes/header.php';?>
+
+
+
+  <!-- HEADER -->
+  <!-- Banner Start-->
+  <section class="about_banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 admin_banner_left">
+          <h4 class="about_h4">
+            Why Choose Us
+          </h4>
+          <h1 class="about_h1">
+            Elevate your forex<br> game with admin <br>consultation bliss!
+          </h1>
+          <p class="about_p">
+            Unleash the full potential of your forex endeavors with our admin consultation services. Let our experts handle the backend, while you focus on your trading success. Seamlessly optimize your operations and maximize your profits!
+          </p>
+          <div class="cta_unique">
+            <a class="unique_anchor_one" href="#">Request a Demo </a>
+            <a class="unique_anchor" href="#">Start Broker </a>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="images/FreeService-Hero.png" width="100%">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Banner End -->
+
+<!-- Why YaPrime Start-->
+
+  <section class="why_yaprime">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="unique_heading">Why YaPrime</h2>
+          <p class="unique_para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+      <div class="row row_two">
+        <div class="col-md-6">
+          <div class="why_pointers">
+            <img src="images/dollar.png" class="why_img">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="why_pointers">
+            <img src="images/dollar.png" class="why_img">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row row_two">
+        <div class="col-md-6">
+          <div class="why_pointers">
+            <img src="images/dollar.png" class="why_img">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="why_pointers">
+            <img src="images/dollar.png" class="why_img">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- Why YaPrime END -->
+
+
+  <!-- TALK TO US -->
+  <?php include 'includes/talk-to-us.php';?>
+
+  <!-- TALK TO US -->
+
+  <!-- FOOTER -->
+<?php include 'includes/footer.php';?>
+
+  <!-- FOOTER -->
+</body>
+
+</html>
