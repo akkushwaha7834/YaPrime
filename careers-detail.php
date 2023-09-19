8f722cac50708786e7c343fd520d5f4f62440c01
akkushwaha7834 <akkushwaha018@gmail.com>
2023-09-19 18:01:22 +0530

    caereers page started

---
+++ careers-detail.php
@@ -0,0 +1,168 @@
+<?php include 'includes/header.php';?>
+
+
+<!-- Banner Start-->
+
+	<section class="career_detail_banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="inner_heading">Careers Detail </h1>
+					<p class="inner_para">Lorem ipsum dolor sit amet consectetur. Donec est convallis.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<!-- Banner End -->
+
+<!-- Careers Detail Start -->
+
+<section class="details_section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="images/career-detail.png" width="100%">
+			</div>
+			<div class="col-md-6">
+				<h2 class="details_heading">
+					Are you what we looking for ?
+					<span class="span_detail">Full-stack Developer</span>
+				</h2>
+				<p>We are looking for a Full-stack Developer to join our team. If you love to code and solve problems, then you are on the right track. You will develop our front-end and back-end applications of our product.</p>
+				<div class="counter_details">
+					<div id="countdown">
+				        <div class="days_timer">
+				            <span id="days"></span>
+				            <div class="smalltext">Days</div>
+				        </div>
+				        <div class="hours_timer">
+				            <span id="hours"></span>
+				            <div class="smalltext">Hours</div>
+				        </div>
+				        <div class="minutes_timer">
+				            <span id="minutes"></span>
+				            <div class="smalltext">Minutes</div>
+				        </div>
+				        <div class="seconds_timer">
+				            <span id="seconds"></span>
+				            <div class="smalltext">Seconds</div>
+				        </div>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container container_two">
+		<div class="row">
+			<div class="col-md-8">
+				<h2 class="jd_heading">Job Description</h2>
+				<p class="jd_para">
+					Ut posuere semper malesuada. Praesent in mauris tristique, laoreet mi ac, volutpat enim. Curabitur pharetra sed erat nec molestie. Maecenas id luctus ex. Curabitur porta congue dui sit amet pulvinar. Suspendisse sagittis odio sed urna congue hendrerit. In ante quam, tincidunt et eros tempus, pulvinar tristique libero. In fermentum, velit eu efficitur finibus, metus sem lobortis lorem, a commodo lectus enim eu nisl. Duis quis metus neque. Sed hendrerit diam non justo aliquet, et ultrices erat consectetur. Proin pharetra augue at urna blandit, a porttitor urna viverra. Pellentesque cursus elit vitae libero dapibus, id sollicitudin metus bibendum. Integer lobortis aliquet metus, et iaculis nunc tempor non. Sed imperdiet maximus sodales. Nunc placerat eros leo, eu interdum erat rhoncus id.
+				</p>
+				<h3 class="jd_heading">Who Are We Looking For?</h3>
+				<p class="jd_para">
+					Ut posuere semper malesuada. Praesent in mauris tristique, laoreet mi ac, volutpat enim. Curabitur pharetra sed erat nec molestie. Maecenas id luctus ex. Curabitur porta congue dui sit amet pulvinar. Suspendisse sagittis odio sed urna congue hendrerit. In ante quam, tincidunt et eros tempus, pulvinar tristique libero. In fermentum, velit eu efficitur finibus, metus sem lobortis lorem, a commodo lectus enim eu nisl. Duis quis metus neque. Sed hendrerit diam non justo aliquet, et ultrices erat consectetur. Proin pharetra augue at urna blandit, a porttitor urna viverra.
+				</p>
+				<ul class="who_looking">
+					<li class="who_list">
+						Etiam sit amet risus euismod, vehicula diam quis, fringilla nulla. In scelerisque tincidunt eros eget congue. Aliquam quis augue a nibh pretium feugiat sed vel libero
+					</li>
+					<li class="who_list">
+						Etiam sit amet risus euismod, vehicula diam quis, fringilla nulla. In scelerisque tincidunt eros eget congue. Aliquam quis augue a nibh pretium feugiat sed vel libero
+					</li>
+					<li class="who_list">
+						Etiam sit amet risus euismod, vehicula diam quis, fringilla nulla. In scelerisque tincidunt eros eget congue. Aliquam quis augue a nibh pretium feugiat sed vel libero
+					</li>
+					<li class="who_list">
+						Etiam sit amet risus euismod, vehicula diam quis, fringilla nulla. In scelerisque tincidunt eros eget congue. Aliquam quis augue a nibh pretium feugiat sed vel libero
+					</li>
+				</ul>
+				<h4 class="jd_heading">Our Criteria For This Position</h4>
+				<ul class="ul_list">
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+				</ul>
+				<ul class="ul_list">
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li class="li_list">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+				</ul>
+				<a class="mt4_anchor" href="#">Apply Now </a>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_details_sticky_sidebar">
+				<h3 class="careers_heading">
+					<span>Job Summary</span>
+				</h3>
+				<div class="details_image_box">
+					<img src="images/customer-service.jpg">
+					<h5>Department</h5>
+					<p>Financial Planner</p>
+				</div>
+				<div class="details_image_box">
+					<img src="images/customer-service.jpg">
+					<h5>Department</h5>
+					<p>Financial Planner</p>
+				</div>
+				<div class="details_image_box">
+					<img src="images/customer-service.jpg">
+					<h5>Department</h5>
+					<p>Financial Planner</p>
+				</div>
+				<div class="details_image_box">
+					<img src="images/customer-service.jpg">
+					<h5>Department</h5>
+					<p>Financial Planner</p>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Careers Detail End -->
+
+
+ <!-- TALK TO US -->
+  <?php include 'includes/talk-to-us.php';?>
+
+  <!-- TALK TO US -->
+
+  <!-- FOOTER -->
+<?php include 'includes/footer.php';?>
+
+  <!-- FOOTER -->
+  
+    <script>
+    // Set the target date 10 days from now
+    var targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 10);
+
+    function updateCountdown() {
+        var currentDate = new Date();
+        var timeDifference = targetDate - currentDate;
+
+        if (timeDifference <= 0) {
+            document.getElementById("countdown").innerHTML = "Time's up!";
+        } else {
+            var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+            document.getElementById("days").innerHTML = days;
+            document.getElementById("hours").innerHTML = hours;
+            document.getElementById("minutes").innerHTML = minutes;
+            document.getElementById("seconds").innerHTML = seconds;
+        }
+    }
+
+    setInterval(updateCountdown, 1000); // Update every 1 second
+</script>
+</body>
+
+</html>
