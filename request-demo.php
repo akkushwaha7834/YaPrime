03e9092f0c8815945bd4de5519196c31eb0cdbc0
akkushwaha7834 <akkushwaha018@gmail.com>
2023-10-09 10:29:20 +0530

    Created a new page request-demo.php

---
+++ request-demo.php
@@ -0,0 +1,103 @@
+<?php include 'includes/header.php';?>
+
+<section class="liquidity_banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 admin_banner_left">
+          <h4 class="liquidity_h4">
+            Request a Demo
+          </h4>
+          <h1 class="liquidity_h1">
+          Create Your Prime Account
+          </h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<section class="request_demo_content">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="images/illustrations.png" width="100%">
+      </div>
+      <div class="col-md-6">
+        <form>
+                <div class="form-row form-group">
+                  <div class="col-md-6">
+                      <label for="formGroupExampleInput">Full Name</label>
+                      <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name" required="" name="name" data-gtm-form-interact-field-id="1">
+                  </div>
+                  <div class="col-md-6">
+                      <label for="formGroupExampleInput2">Email Address</label>
+                      <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email Address" required="" name="email" data-gtm-form-interact-field-id="2">
+                  </div>
+                </div>
+                <div class="form-row form-group">
+                  <div class="col-md-6">
+                    <label for="inputState">Enter Contact Number</label>
+                    <input type="text" placeholder="Enter Contact Number" name="contact_number" class="form-control">
+                  </div>
+                  <div class="col-md-6">
+                    <label for="inputState">Your Country</label>
+                    <input type="text" placeholder="Your Country" name="country" class="form-control">
+                  </div>
+                </div>
+                <div class="form-row form-group">
+                  <div class="col-md-12">
+                      <label for="inputState">Are You A ?</label>
+                      <select class="form-control" id="areyoua" name="areyoua" required="">
+                          <option value="">Select</option>
+                          <option value="Start Up">Start Up</option>
+                          <option value="Introducing Broker">Introducing Broker</option>
+                          <option value="Existing Broker">Existing Broker</option>
+                          <option value="Others">Others</option>
+                      </select>
+                  </div>
+                </div>
+                <div class="form-row form-group">
+                  <div class="col-md-12">
+                    <label for="inputState">What are the product or services you looking for?</label>
+                      <select class="form-control" id="products" name="products" required="">
+                          <option disabled="" value="" selected="">Select Products</option>
+                          <option value="Traders Room">Traders Room</option>
+                          <option value="Forex Brokerage">Forex Brokerage</option>
+                          <option value="MT5 White Label">MT5 White Label</option>
+                          <option value="MT5 ">MT5 </option>
+                          <option value="Forex Websites Broker">Forex Websites Broker</option>
+                          <option value="Forex CRM">Forex CRM</option>
+                          <option value="Forex Liquidity">Forex Liquidity</option>
+                          <option value="MAM">MAM</option>
+                          <option value="MAM/PAMM Support">MAM/PAMM Support</option>
+                          <option value="PAMM">PAMM</option>
+                          <option value="Merchant Services">Merchant Services</option>
+                          <option value="Deposits And Withdrawals With Bitcoin-ETH-USDT">Deposits And Withdrawals
+                              With
+                              Bitcoin-ETH-USDT</option>
+                          <option value="IB &amp; Affiliate Area">IB &amp; Affiliate Area</option>
+                          <option value="All The Above">All The Above</option>
+                      </select>
+                  </div>
+                </div>
+                <div class="form-row form-group">
+                  <div class="col-md-12">
+                    <label for="inputState">Do you need a quote ?</label>
+                      <select class="form-control" id="qoute" name="qoute" required="">
+                          <option value="">Select</option>
+                          <option value="Yes">Yes</option>
+                          <option value="No">No</option>
+                      </select>
+                  </div>
+                </div>
+                  <div class="form-group">
+                      <label for="exampleFormControlTextarea1">Give me a brief about your requirement</label>
+                      <textarea class="form-control" id="exampleFormControlTextarea1"rows="1" cols="50" placeholder="Message" name="message"></textarea>
+                  </div>
+                  <button type="submit" class="unique_anchor_one btn btn-primary">Submit</button>
+              </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include 'includes/footer.php';?>
