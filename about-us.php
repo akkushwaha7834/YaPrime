36790306984078a0def5c973292db1ac5a7fcfa2
akkushwaha7834 <akkushwaha018@gmail.com>
2023-10-10 10:39:52 +0530

    About Us Content Updated

--- about-us.php
+++ about-us.php
@@ -12,7 +12,7 @@
             About Us
           </h4>
           <h1 class="about_h1">
-            YaPrime: Your Top Tier Forex Liquidity Provider
+            YaPrime: Inspiring Future Forex Brokers
           </h1>
           <p class="about_p">
             At YaPrime, our core belief is to inspire and empower aspiring Forex brokers with our premium liquidity, brokerage, and trading technology solutions. We are dedicated to shaping the future of Forex trading through innovation and excellence.
@@ -36,20 +36,66 @@
       <div class="row">
         <div class="col-md-6 benefits_left">
           <h3 class="benefits_h3">
-            YaPrime
+            ABOUT US
           </h3>
           <h2 class="benefits_h2">
-            Mission
+            Bespoke Liquidity Solutions Provider
           </h2>
           <p class="benefits_p">
-           Our Mission is to dominate the Forex Industry by giving our broker partners best-in-class liquidity solutions, state-of-the-art technology, and endless support, all geared toward their ongoing prosperity in a continually evolving market. We are the Top Tier Liquidity Providers aiming to raise the bar higher. We consistently advance our technology and tools to supply the best solutions possible for the most successful FX brokerages.
+           YaPrime, a brand of Ya Group Limited, is a forex and CFD liquidity solutions provider focused on institutional clients. It was founded by industry veterans with deep knowledge of the retail and institutional sectors. YaPrime's team of experts and broker consultants helps clients set up their own forex brokerage companies and provides exceptional liquidity solutions and unparalleled support.
           </p>
         </div>
         <div class="col-md-6">
           <img src="images/mt5.png" width="100%">
         </div>
       </div>
-
+      <div class="row about_row_two_intro">
+        <div class="col-md-6">
+          <img src="images/mt5.png" width="100%">
+        </div>
+        <div class="col-md-6 benefits_left">
+          <h3 class="benefits_h3">
+            YAPRIME
+          </h3>
+          <h2 class="benefits_h2">
+            Our Mission
+          </h2>
+          <p class="benefits_p">
+           To revolutionise the forex industry by giving our partners and institutional clients best-in-class liquidity solutions, exceptional trading technology, and endless support, all geared towards their ongoing growth in a constantly evolving market. With a focus on innovation, we strive to develop and provide advanced bespoke forex liquidity solutions to enable our clients to achieve unprecedented growth.
+          </p>
+        </div>
+      </div>
+      <div class="row about_row_two_intro">
+        <div class="col-md-6 benefits_left">
+          <h3 class="benefits_h3">
+            YAPRIME 
+          </h3>
+          <h2 class="benefits_h2">
+            Our Unique Approach
+          </h2>
+          <p class="benefits_p">
+           We understand that every client is unique, so we offer customised solutions catering to their needs. When you partner with YaPrime, you receive more than just liquidity services. Our team of experts works closely with our clients to offer innovative ideas, tools for easier operations, risk management solutions, dealing desk strategies, marketing leads, and much more. We constantly innovate and improve our services to stay ahead of the curve and deliver solutions that help our clients succeed in the ever-changing financial landscape.
+          </p>
+        </div>
+        <div class="col-md-6">
+          <img src="images/mt5.png" width="100%">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="strong_regulatory_section">
+    <div class="container">
+      <div class="row about_row_two_intro_three">
+        <div class="col-md-12">
+          <h3>Strong Regulatory Framework</h3>
+          <p class="benefits_p">
+            YaPrime is regulated under the Mauritius Financial Services Authority(MFSA). We adhere to strict regulatory guidelines, ensuring our operations' transparency, security, and professionalism. Our commitment to compliance is a core pillar of our business, and we continuously work to maintain the highest in every aspect of our services. 
+          </p>
+          <p class="benefits_p">
+            Our regulation under the MFSA guarantees that we operate within a well-established and secure legal framework. This gives our clients the confidence and trust to partner with us for their liquidity solutions. We prioritise the security of our client's data and assets, implementing robust internal controls and procedures to safeguard sensitive information and client funds.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
   <!-- Benefits End   -->
@@ -66,25 +112,16 @@
         <div class="col-md-3">
           <img src="images/icon1.png">
           <h4 class="icon_box_heading">
-            Grow Your Forex Business
+            Focused on Growth
           </h4>
           <p class="icon_box_para">
-            We're dedicated to boosting your forex business by delivering efficiencies that benefit both you and your clients.
+            Experience exponential growth in your forex business with our perfect brokerage solutions that will benefit you and your clients.
           </p>
         </div>
         <div class="col-md-3">
           <img src="images/icon1.png">
           <h4 class="icon_box_heading">
-            Access Global Liquidity Pools
-          </h4>
-          <p class="icon_box_para">
-            Our custom liquidity solutions leverage fast, robust technology and institutional risk management and execution, giving you access to global liquidity pools.
-          </p>
-        </div>
-        <div class="col-md-3">
-          <img src="images/icon1.png">
-          <h4 class="icon_box_heading">
-            Scale Your Trading Offering
+            Scaling Your Business
           </h4>
           <p class="icon_box_para">
             With the ability to distribute and source deals globally across five continents, we enable you to scale your trading offering and expand your reach.
@@ -93,10 +130,19 @@
         <div class="col-md-3">
           <img src="images/icon1.png">
           <h4 class="icon_box_heading">
-            Simplify Cost and Complexity
+            Simplifying Complexities
           </h4>
           <p class="icon_box_para">
             Reduce cost and complexity by leveraging our third-party integrations and trading infrastructure. This allows you to focus on delivering your core value.
+          </p>
+        </div>
+        <div class="col-md-3">
+          <img src="images/icon1.png">
+          <h4 class="icon_box_heading">
+            Global Liquidity Pools
+          </h4>
+          <p class="icon_box_para">
+            Our custom liquidity solutions equip you with fast and robust trading technology, institutional risk management, and access to global liquidity pools.
           </p>
         </div>
       </div>
