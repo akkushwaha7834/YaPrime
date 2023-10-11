b0e7602390c2a4e7da2b39a40556a6b91f6ad0bb
akkushwaha7834 <akkushwaha018@gmail.com>
2023-10-11 11:25:32 +0530

    Added Meta k/w and description tags in all pages

--- admin-consultation.php
+++ admin-consultation.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>YaPrime</title>
+    <meta name="description" content=""/>
+    <meta name="keywords" content="" />
     <link rel="icon" type="image/x-icon" href="images/favicon.png">
     <link rel="stylesheet" type="text/css" href="css/bootstrap5.3.0.min.css">
     <link rel="stylesheet" type="text/css" href="css/material-design-iconic-font.min.css">
